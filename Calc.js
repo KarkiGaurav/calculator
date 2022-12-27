41d2d28508da11536ee1bf61ce8e79b2ff671d55
Gaurav <karkigauravsingh@gmail.com>
2022-12-27 11:28:59 +0530

    first commit

---
+++ Calc.js
@@ -0,0 +1,14 @@
+// var clr = document.getElementById("clear");
+
+// function blank(){
+//     // alert("this is working");
+//     display.value = "";
+// }
+
+// clr.addEventListener("click", blank);
+
+
+function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
